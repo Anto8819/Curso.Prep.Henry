75b331919a62f12945d250fda4e2c3ad0c20d3cb
Antonella Rapuano <mrap1988@gmail.com>
2021-07-07 17:16:09 -0300

    Code optimization

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -52,14 +52,14 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  const newarr = [];  
-  for (let i = 0; i < array.length; i ++){
-    if (array[i][0] === 'a') {
-      newarr.push(array[i]) 
+  var newarr = []
+  array.forEach(element => {
+    if(element[0] === 'a') {
+      newarr.push(element)
     }
-  }
+  });
   return newarr;
-}
+  }
 
 // No modificar nada debajo de esta línea
 // --------------------------------
